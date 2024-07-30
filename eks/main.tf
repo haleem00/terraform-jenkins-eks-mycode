44ed59f95792cdb1f859896ae358ef2e92b51c47
haleem00 <mahmoudhaleem100@gmail.com>
2024-07-30 18:20:02 +0400

    change the name of the cluster

--- eks/main.tf
+++ eks/main.tf
@@ -67,7 +67,7 @@ module "vpc" {
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "my-cluster"
+  cluster_name    = "my-cluster-terraform"
   cluster_version = "1.24"
   
   cluster_endpoint_public_access = true
